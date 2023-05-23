417e989eea1589f1461a5022d23b34fbc087a5c9
NormanSackett <133908393+NormanSackett@users.noreply.github.com>
2023-05-23 14:09:09 -0400

    created property
    Property shell and isOwned mutater and accessor

---
+++ Property.java
@@ -0,0 +1,23 @@
+
+public class Property extends Object {
+	int costProperty;
+	int numHouses;
+	int numHotels;
+	boolean isOwned;
+	int rent;
+	boolean isMortgaged;
+	String colorSet;
+	int propertyOneHouse;
+	int propertyTwoHouses;
+	int propertyThreeHouses;
+	int propertyFourHouses;
+	int propertyHotel;
+	
+	public boolean getIsOwned() {
+		return isOwned;
+	}
+	
+	public void setIsOwned(boolean owned) {
+		isOwned = owned;
+	}
+}
