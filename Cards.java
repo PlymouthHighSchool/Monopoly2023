92cb249ae8ca3443a5c11f8da3933f9ad2feba5e
Xander Tirrell <xander@tirrell.com>
2023-05-25 14:13:17 -0400

    Create Cards.java

---
+++ Cards.java
@@ -0,0 +1,27 @@
+package Monopoly;
+
+public class Cards {
+	String message;
+	int[] thePosition;
+	int venmo;
+	int payout;
+	Boolean getOutOfJail;
+	int houseCharge;
+	int hotelCharge;
+
+	public Cards() {
+		
+	}
+	
+	
+	public String DoAction() {
+		return "";
+		
+	}
+	
+	private int calculatePayout() {
+		return 0;
+		
+	}
+}
+
