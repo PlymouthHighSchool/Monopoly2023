af7aacc6853bc6ee77927997b51c1b79129a926d
DylanScammon45 <133908374+DylanScammon45@users.noreply.github.com>
2023-05-25 14:10:40 -0400

    Create calculateTax.java
    calculates the tax of the space landed on with a parameter of the current player

---
+++ calculateTax.java
@@ -0,0 +1,18 @@
+	public int calculateTax(Player thePlayer) {
+		String[] taxOptions = new String[2];
+		taxOptions[0] = "Pay $200";
+		taxOptions[1] = "Pay 10% of Worth";
+		int playerTax = Math.round(thePlayer.getTotalWorth() * .1);
+		if (thePlayer.getLocation() == 4) {
+			if (Interface.createOptionPane(taxOptions, "Would you like to pay $200 or pay 10% of your total worth?", "Choose your payment") == 0) {
+				payTax = 200;
+			}
+			else
+				payTax = playerTax;
+		}
+		else if (thePlayer.getLocation() == 38) {
+			payTax = 75;
+		}
+		return payTax;
+	}
+	
