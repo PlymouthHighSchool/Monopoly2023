79dd8cd75a93e104149e93799a7d28868eb0d3de
MylesKentPatterson <133908518+MylesKentPatterson@users.noreply.github.com>
2023-05-25 13:57:48 -0400

    Update endTurnClick.java

--- endTurnClick.java
+++ endTurnClick.java
@@ -1,10 +1,3 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-
-public class endTurnClick {
-
-	public static void main(String[] args) {
 		JButton endTurnClick = new JButton("End Turn");
 		endTurnClick.setBounds(0,0, 0,0);//must position
 		endTurnClick.setVisible(true);
@@ -13,7 +6,3 @@ public class endTurnClick {
 					Turn.endTurn()
 		}
 		});
-		
-	}
-	
-}
