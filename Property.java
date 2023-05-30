0d9efae0652aca140d87f0224363be1ffea7ecad
GordonLove <1gordoncooperlove@gmail.com>
2023-05-30 12:22:29 -0400

    Update Property.java

--- Property.java
+++ Property.java
@@ -1,6 +1,25 @@
 
-public class Property {
+public class Property extends Object {
+	int costProperty;
+	int numHouses;
+	int numHotels;
+	boolean isOwned;
+	int rent;
+	boolean isMortgaged;
+	String colorSet;
+	int propertyOneHouse;
+	int propertyTwoHouses;
+	int propertyThreeHouses;
+	int propertyFourHouses;
+	int propertyHotel;
 
+	public boolean getIsOwned() {
+		return isOwned;
+	}
+
+	public void setIsOwned(boolean owned) {
+		isOwned = owned;
+	}
 public Property() {
 	costProperty = 0;
 	numHouses = 0;
@@ -9,10 +28,10 @@ public Property() {
 	rent = 0;
 	isMortgaged = false;
 	colorSet = "";
-	propertyOnehouse = 0;
-	propertyTwoHouse = 0;
-	propertyThreeHouse = 0;
-	propertFourHouse = 0;
+	propertyOneHouse = 0;
+	propertyTwoHouses = 0;
+	propertyThreeHouses = 0;
+	propertyFourHouses = 0;
 	propertyHotel = 0;
 	
 
