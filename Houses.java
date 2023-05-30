4aa1aa5d4901850bdb8e9da23b5dd5fd9c34a0a9
GordonLove <1gordoncooperlove@gmail.com>
2023-05-30 12:30:20 -0400

    Update Houses.java

--- Houses.java
+++ Houses.java
@@ -4,4 +4,14 @@ public void sellHouse() {
 	Player.changeWallet((getCost() * -1));
 	numHotels--;
 }
+
+int totalBuildingLimit;
+
+Houses() {
+	totalBuildingLimit = 32;	
+}
+
+public int getBuildingLimit() {
+	return totalBuildingLimit; 
+}
 }
