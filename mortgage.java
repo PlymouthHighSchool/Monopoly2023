d6a727a5301ee48b5268fa904816d45b6b09d6f8
MylesKentPatterson <133908518+MylesKentPatterson@users.noreply.github.com>
2023-05-31 10:23:34 -0400

    Update mortgage.java

--- mortgage.java
+++ mortgage.java
@@ -1,9 +1,17 @@
-package mortgage;
-
-public class mortgage {
-
-	public static void main(String[] args) { 
-		
-	Game.getArrSpace()[getPlayerInfo().getLocation()];
+	private static int mortgage(Property theProperty) {
+		int tempNum;
+		if (theProperty.getMortgaged() == false) {
+			if (theProperty.getNumHotels == 1) {
+				tempNum = (theProperty.getCostProperty() + (theProperty.getBuildingCost() * 5)) / 2;
+			}
+			else {
+				tempNum = (theProperty.getCostProperty() + (theProperty.getBuildingCost() * theProperty.getNumHouses)) / 2;
+			}
+			
+			theProperty.setMortgaged(true);
+			return tempNum;
+		}
+		else {
+			return 0;	
+		}
 	}
-}
