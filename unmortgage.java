bc35842f79bbaa428d24e689492b752be0c43db5
MylesKentPatterson <133908518+MylesKentPatterson@users.noreply.github.com>
2023-05-31 10:50:34 -0400

    unmortgage
    returns value that needs to be paid to unmortgage the property passed into it and unmortgages it as well

---
+++ unmortgage.java
@@ -0,0 +1,19 @@
+public class unmortgage {
+	private static int unmortgage(Property theProperty) {
+		int tempNum;
+		if (theProperty.getMortgaged() == false) {
+			if (theProperty.getNumHotels == 1) {
+				tempNum = (theProperty.getCostProperty() + (theProperty.getBuildingCost() * 5)) / 2;
+			}
+			else {
+				tempNum = (theProperty.getCostProperty() + (theProperty.getBuildingCost() * theProperty.getNumHouses)) / 2;
+			}
+			tempNum *= 1.1;
+			theProperty.setMortgaged(false);
+			return tempNum;
+		}
+		else {
+			return 0;	
+		}
+	}
+}
