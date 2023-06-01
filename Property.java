3f8ab720dc3ed43ea961dc7ab995da50008a0f0e
Christine Kuan <99509219+christinekuan8838@users.noreply.github.com>
2023-06-01 13:44:01 -0400

    Update Property.java
    Finished "public int getRent()" method and tested it. Description of method: "returns the cost of landing on the property"

--- Property.java
+++ Property.java
@@ -13,6 +13,26 @@ public class Property extends Object {
 	int propertyFourHouses;
 	int propertyHotel;
 	
+	public int getRent() { //returns the cost of landing on the property
+		if (numHotels == 1) {
+			return propertyHotel;
+		}
+		else if (numHouses == 1) { 
+			return propertyOneHouse; 
+		}
+		else if (numHouses ==2) {
+			return propertyTwoHouse;
+		}
+		else if (numHouses == 3) {
+			return propertyThreeHouse;
+		}
+		else if (numHouses == 4) {
+			return propertyFourHouse;
+		}
+		else {
+			return  0;
+		}
+		
 	public boolean getIsOwned() {
 		return isOwned;
 	}
