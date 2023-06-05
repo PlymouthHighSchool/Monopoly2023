0445f0012a5ef4a6905f676d8e92ddacab1e6bec
The-Evan-Duchette <133908478+The-Evan-Duchette@users.noreply.github.com>
2023-06-05 13:32:08 -0400

    Delete endTurnClick.java

--- endTurnClick.java
+++
@@ -1,8 +0,0 @@
-		JButton endTurnClick = new JButton("End Turn");
-		endTurnClick.setBounds(0,0, 0,0);//must position
-		endTurnClick.setVisible(true);
-		endTurnClick.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-					Turn.endTurn()
-		}
-		});
