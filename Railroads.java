a40cc45837b20d5b21b926cb7b6763fcd3af579d
Xander Tirrell <xander@tirrell.com>
2023-06-05 13:48:21 -0400

    Update Railroads.java
    Railroad shell, constructor, and calcRent

--- Railroads.java
+++ Railroads.java
@@ -1,9 +1,29 @@
+package Monopoly;
+
 public class Railroads extends Property {
-	public Railroads(int cost) {
-		costProperty = cost;
+	
+	String railroadName;
+	
+	public Railroads(String name) {
+		costProperty = 200;
+		railroadName = name;
 	}
 	
-	public static void calcRent(int localRent) {
-
+	public int calcRent(int numOfOwnedRailroads) {
+		rent = 25;
+		if (numOfOwnedRailroads == 2) {
+			rent = 50;
+		}
+		else if (numOfOwnedRailroads == 3) {
+			rent = 100;
+		}
+		else {
+			rent = 200;
+		}
+		return rent;
+	}
+	
+	public String getRailroadName() {
+		return railroadName;
 	}
 }
