9f5d9ae2b217db8122d261ad85646c98e21f6720
Maddoxx14 <133908554+Maddoxx14@users.noreply.github.com>
2023-06-05 13:07:32 -0400

    Update Player.java
    Add getTotalWorth and changeWallet to the player class.

--- Player.java
+++ Player.java
@@ -1,5 +1,6 @@
 
-public class Player{
+
+public class Player {
 	int wallet;
 	int location;
 	String piece;
@@ -8,7 +9,7 @@ public class Player{
 	Boolean inJail ;
 	int timeInJail;
 	Property myProperties[] = new Property()[22];
-	
+ 
 	public int numOfHotels() {
 		int myAmount;
 		for(int count = 0; count < 23; count++ ) {
@@ -36,12 +37,34 @@ public class Player{
 		
 		return  myAmount ;
 	}
-	
 	public int calcTax(int totalWorth) {
 		totalWorth = (int) (totalWorth * .9);
 		return totalWorth;
 		
 	}
-
 	
+	public void changeWallet(int money) {
+	 if (wallet+money>0) {
+		wallet=wallet+money; 
+	 }
+	 else {
+		 generateFunds(-1*(wallet+money));
+	 }
+ }
+ 	public int getTotalWorth() {
+ 		int valofHouses; 
+ 		int valofHotels; 
+ 		int valofProperties;
+ 		int totalWorth;
+ 		int currentPlayer= Player.playerNumber;
+ 		for(int property=0;property<=22;property++) {
+ 			if(myProperties[property].isOwnedBy==currentPlayer) {
+ 				valofProperties=valofProperties+myProperties[property].costProperty;
+ 				valofHouses=valofHouses+(myProperties[property].numHouses*myProperties[property].costOfBuilding);
+ 				valofHotels=valofHotels+(myProperties[property].numHotels*myProperties[property].costOfBuilding);
+ 				totalWorth=totalWorth+valofProperties+valofHouses+valofHotels;
+ 			}
+ 		}
+ 		return totalWorth;
+ 	}
 }
