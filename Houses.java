017eb0c0fbccea8b45b396fa7ec0bfd519326e88
Maddoxx14 <133908554+Maddoxx14@users.noreply.github.com>
2023-06-05 14:18:52 -0400

    Update Houses.java
    Added comment to returnHouses to specify use.

--- Houses.java
+++ Houses.java
@@ -10,7 +10,7 @@ int totalBuildingLimit;
 	public int getBuildingLimit() {
 		return totalBuildingLimit; 
 	}
-	public void returnHouses(Property currentProperty) {
+	public void returnHouses(Property currentProperty) { //When a hotel on a property is sold, four houses are taken from the total and put in its place
 		currentProperty.numofHouses=4;
 		totalBuildingLimit=totalBuildingLimit-4;
 	}
