b3114e836258bcb7b7d9bf23c12eb3edb2dfe43a
The-Evan-Duchette <133908478+The-Evan-Duchette@users.noreply.github.com>
2023-06-06 10:40:22 -0400

    Update Turn.java

--- Turn.java
+++ Turn.java
@@ -26,6 +26,23 @@ private static int unmortgage(Property theProperty) {
 		return 0;	
 	}
 }
+private static int mortgage(Property theProperty) {
+	int tempNum;
+	if (theProperty.getMortgaged() == false) {
+		if (theProperty.getNumHotels == 1) {
+			tempNum = (theProperty.getCostProperty() + (theProperty.getBuildingCost() * 5)) / 2;
+		}
+		else {
+			tempNum = (theProperty.getCostProperty() + (theProperty.getBuildingCost() * theProperty.getNumHouses)) / 2;
+		}
+		
+		theProperty.setMortgaged(true);
+		return tempNum;
+	}
+	else {
+		return 0;	
+	}
+}
 
 	private static int doubleCount, rollNum;
 	private static Dice[] dice = new Dice[2];
