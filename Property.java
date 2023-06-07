dac325ecf4c31eb763fccaa954984b42aa304f31
GordonLove <1gordoncooperlove@gmail.com>
2023-06-07 10:29:59 -0400

    Update Property.java

--- Property.java
+++ Property.java
@@ -5,7 +5,7 @@ public class Property extends Object {
 	int numHotels;
 	boolean isOwned;
 	int rent;
-	boolean isMortgaged;
+	boolean isMortgaged; //add player number and tunr order and use p-g's constructor edit fro it
 	String colorSet;
 	String name;
 	int propertyOneHouse;
@@ -13,39 +13,65 @@ public class Property extends Object {
 	int propertyThreeHouses;
 	int propertyFourHouses;
 	int propertyHotel;
-
-	public boolean getIsOwned() {
-		return isOwned;
-	}
-
-	public void setIsOwned(boolean owned) {
-		isOwned = owned;
-	}
+	int costOfBuilding;
 	
-	public String getColor() {
-		return colorSet;
-}
-public Property(String name, int costProperty, int rent,int propertyOneHouse, int propertyTwoHouses, int propertyThreeHouses,int propertyFourHouses,int propertyHotel,String colorSet) {
-	costProperty = costProperty;
+public Property(String varName, int varCostProperty, int varRent,int varPropertyOneHouse, int varPropertyTwoHouses, int varPropertyThreeHouses,int varPropertyFourHouses,int varPropertyHotel,String varColorSet) {
+	costProperty = varCostProperty;
 	numHouses = 0;
 	numHotels = 0;
 	isOwned = false;
-	rent = rent;
+	rent = varRent;
 	isMortgaged = false;
-	propertyOneHouse = propertyOneHouse;
-	propertyTwoHouses = propertyTwoHouses;
-	propertyThreeHouses = propertyThreeHouses;
-	propertyFourHouses = propertyFourHouses;
-	propertyHotel = propertyHotel;
-	name = name;
-	colorSet = colorSet;
+	propertyOneHouse = varPropertyOneHouse;
+	propertyTwoHouses = varPropertyTwoHouses;
+	propertyThreeHouses = varPropertyThreeHouses;
+	propertyFourHouses = varPropertyFourHouses;
+	propertyHotel = varPropertyHotel;
+	name = varName;
+	colorSet = varColorSet;
 
 }
+
+public boolean getIsOwned() {
+		return isOwned;
+
+	}
+public void setIsOwned(boolean owned) {
+		isOwned = owned;
+	}
+	
+public String getColor() {
+		return colorSet;
+}		
 public int getNumHouses() {
 	return numHouses;
 }
 
 public int getNumHotels() {
 	return numHotels;
+}
+
+public int getBuildingCost() {
+	return costOfBuilding;
+}
+
+public int getCostProperty() {
+	return costProperty;
+}
+
+public void setMortgaged(Boolean mortgaged) {
+	isMortgaged = mortgaged;
+}
+
+public void setNumHotels(int inNumHotels) {
+	numHotels = inNumHotels;
+}
+
+public void setNumHouses(int inNumHouses) {
+	numHouses = inNumHouses;
+}
+
+public boolean getMortgaged() {
+	return isMortgaged;
 }
 }
