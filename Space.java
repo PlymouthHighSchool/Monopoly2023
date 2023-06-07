1743e9015142f947a00860392920e04f430e19be
williamSmoker24 <134436789+williamSmoker24@users.noreply.github.com>
2023-06-07 13:13:46 -0400

    Update Space.java added variables and get space name

--- Space.java
+++ Space.java
@@ -1,9 +1,16 @@
 
 public class Space {
+	String nameOfSpace;
+	int location;
+	Player isOwnedBy;
+	Boolean isOwned;
 public Space(int location, String nameSpace) {
 	nameOfSpace = nameSpace;
 	location = location;
 }
+	public String getSpaceName(){
+		return(nameOfSpace);
+	}
 	public int getLocation(){
 		return location;
 	}
