84fd28e25ea80659793b62182d1381eb6692f8cd
GordonLove <133908327+GordonLove@users.noreply.github.com>
2023-06-09 12:27:29 -0400

    Update Space.java

--- Space.java
+++ Space.java
@@ -1,9 +1,11 @@
 
-public class Space {
-public Space(int location, String nameSpace) {
-	nameOfSpace = nameSpace;
-	location = location;
-}
+public abstract class Space {
+/* // we dont need this given space is abstract
+	public Space(int location, String nameSpace) {
+		nameOfSpace = nameSpace;
+		location = location;
+	}
+	*/
 	public int getLocation(){
 		return location;
 	}
