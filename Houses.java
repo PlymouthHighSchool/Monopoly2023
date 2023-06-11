e7292cc1106ca2ad47a11774da7a30273d70bdfe
GordonLove <133908327+GordonLove@users.noreply.github.com>
2023-06-11 10:26:38 -0400

    Update Houses.java

--- Houses.java
+++ Houses.java
@@ -2,9 +2,8 @@
 
 public class Houses extends Buildings {
 	int totalBuildingLimit; 
-	public void sellHouse(Property house) {
-		Player token = null;
-		token.changeWallet(house.getBuildingCost()/2); //changes the players wallet
+	public void sellHouse(Property house, Player currentPlayer) {
+		currentPLayer.changeWallet(house.getBuildingCost()/2); //changes the players wallet
 		house.setNumHouses(house.numHouses -1); // takes 1 house from that property becuase you have now sold it
 	}
 
